5d412d21499443289895baa26e85b0dc85a636be
raul284 <raulpe07@ucm.es>
2025-03-16 03:53:39 +0100

    mas bonito todo

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var pressed : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,3 +20,23 @@ func GoToOptions() -> void:
 
 func Exit() -> void:
 	get_tree().quit()
+
+
+func HoverPlay() -> void:
+	if !pressed:
+		$AnimatedSprite2D.play("over")
+
+
+func LeftPlay() -> void:
+	if !pressed:
+		$AnimatedSprite2D.play("left")
+
+func PlayUp() -> void:
+	$Timer.start()
+	
+func PlayPress() -> void:
+	pressed = true
+	$Button2/Sprite2D.visible=false
+	$Button2/EmptyCircle.visible=false
+	$AnimatedSprite2D.scale.x = -1*$AnimatedSprite2D.scale.x
+	$AnimatedSprite2D.play("press")
