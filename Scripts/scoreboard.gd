58f674ee84ac36f09587cad673f5fcdb05f3ecd8
Migram05 <migram05@ucm.es>
2025-03-16 22:28:35 +0100

    Patch día uno

--- Scripts/scoreboard.gd
+++ Scripts/scoreboard.gd
@@ -56,14 +56,14 @@ func SaveScore():
 	var num : int = 0
 	var i : int = 0
 	var new_score_registered : bool = false
-	while num < 3:
-		if (i >= content_split.size()-1):
+	while num < 10:
+		if (i >= content_split.size()-1): 
 			if(!new_score_registered):
 				result += ScoreRegister.team_name + " " + str(str(round_to_dec(ScoreRegister.height, 2))) + " " + str(ScoreRegister.moscas_killed) + " " + str(ScoreRegister.ciempieses_killed) + " " + str(ScoreRegister.perfect_notes) + " " + str(ScoreRegister.good_notes) + " " + str(ScoreRegister.meh_notes) + " " + str(ScoreRegister.missed_notes) + "\n" 
 			break
 		var height = float(content_split[i].split(" ")[1])
 		if (new_score_registered or height >= ScoreRegister.height):
-			result += content_split[i]
+			result += content_split[i] + '\n'
 			num += 1
 			i += 1
 		else:
